423ddac476c2a1bf67ce84becd2ef4e05e7e8fa2
Jonathan Xia <fclvbfm934@gmail.com>
2018-03-06 15:10:16 -0800

    Added a vimsetup script

---
+++ vimsetup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle
+git clone https://github.com/jistr/vim-nerdtree-tabs ~/.vim/bundle
+git clone https://github.com/xolox/vim-misc ~/.vim/bundle
+git clone https://github.com/xolox/vim-easytags ~/.vim/bundle
+git clone https://github.com/craigemery/vim-autotag ~/.vim/bundle
+git clone https://github.com/scrooloose/nerdtree ~/.vim/bundle
+git clone https://github.com/majutsushi/tagbar ~/.vim/bundle
+git clone https://github.com/vim-scripts/AutoComplPop ~/.vim/bundle
+git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle
+
