2cf787b701d46312e7938864d9540e462e657464
Jonathan Xia <jonathanxia@berkeley.edu>
2018-03-07 00:02:57 +0000

    Fixed directories in vimsetup.sh

--- vimsetup.sh
+++ vimsetup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle
-git clone https://github.com/jistr/vim-nerdtree-tabs ~/.vim/bundle
-git clone https://github.com/xolox/vim-misc ~/.vim/bundle
-git clone https://github.com/xolox/vim-easytags ~/.vim/bundle
-git clone https://github.com/craigemery/vim-autotag ~/.vim/bundle
-git clone https://github.com/scrooloose/nerdtree ~/.vim/bundle
-git clone https://github.com/majutsushi/tagbar ~/.vim/bundle
-git clone https://github.com/vim-scripts/AutoComplPop ~/.vim/bundle
-git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle
+git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
+git clone https://github.com/jistr/vim-nerdtree-tabs ~/.vim/bundle/vim-nerdtree-tabs
+git clone https://github.com/xolox/vim-misc ~/.vim/bundle/vim-misc
+git clone https://github.com/xolox/vim-easytags ~/.vim/bundle/vim-easytags
+git clone https://github.com/craigemery/vim-autotag ~/.vim/bundle/vim-autotag
+git clone https://github.com/scrooloose/nerdtree ~/.vim/bundle/nerdtree
+git clone https://github.com/majutsushi/tagbar ~/.vim/bundle/tagbar
+git clone https://github.com/vim-scripts/AutoComplPop ~/.vim/bundle/AutoComplPop
+git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
 
