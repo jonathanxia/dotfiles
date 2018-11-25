a74586102792a19e73c08e2d85a2b13b6b06a998
Jonathan Xia <jonathanxia@berkeley.edu>
2018-11-25 13:23:10 -0800

    Added instructions to the readme

--- README.md
+++ README.md
@@ -26,15 +26,10 @@ Run this command to not show untracked files:
 `config config --local status.showUntrackedFiles no`
 
 There should be a shell script that will be used to clone all the repositories for the
-vim plugins. Run
+vim plugins. *You should first check the script*. Read through the comments to see what it is installing
+and comment out lines that should not be run for you (like the `sudo apt-get install`). Run
 
 `./vimsetup.sh`
-
-If you already have the repository, then it'll complain but that's fine since you already
-have it. Also, this assumes you have pathogen working, which you should go and get. I'll
-maybe add in pathogen to automate that too...
-
-Get pathogen here: https://github.com/tpope/vim-pathogen
 
 Add this to your `.bashrc` to get the base16 color scheme working:
 
@@ -43,3 +38,14 @@ BASE16_SHELL="$HOME/.config/base16-shell/"
 [ -n "$PS1" ] && \
     [ -s "$BASE16_SHELL/profile_helper.sh" ] && \
         eval "$("$BASE16_SHELL/profile_helper.sh")"`
+
+Getting this base16 thing to work requires you to have a 256 color terminal. You can probably configure
+this in your terminal settings. You can run
+
+`~/.config/base16-shell/colortest`
+
+to see if you got it to work. The last few lines should *not* be blue.
+
+You may also need to go here: https://github.com/powerline/fonts for the fonts to make powerline work.
+If you see a bunch of question marks on your powerline status bar on the bottom, that means you don't
+have the proper fonts. You'll have to install those...somehow!
--- vimsetup.sh
+++ vimsetup.sh
@@ -1,28 +1,45 @@
 #!/bin/bash
 
-# Add pathogen
+# Add pathogen (rtp manager, necessary)
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
+# Adding Vundle (an rtp manager, also necessary)
+git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
+
+# Makes the bottom look fancy.
 git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
+
+# Gives you the pane on the left with files
+git clone https://github.com/scrooloose/nerdtree ~/.vim/bundle/nerdtree
 git clone https://github.com/jistr/vim-nerdtree-tabs ~/.vim/bundle/vim-nerdtree-tabs
-git clone https://github.com/xolox/vim-misc ~/.vim/bundle/vim-misc
 
 # Install ctags
+# Comment this line out if you already have ctags
 sudo apt-get install exuberant-ctags
 
+# Gives you tags in your vim, with the tag bar on the right, displaying function names
+git clone https://github.com/xolox/vim-misc ~/.vim/bundle/vim-misc
 git clone https://github.com/xolox/vim-easytags ~/.vim/bundle/vim-easytags
 git clone https://github.com/craigemery/vim-autotag ~/.vim/bundle/vim-autotag
-git clone https://github.com/scrooloose/nerdtree ~/.vim/bundle/nerdtree
 git clone https://github.com/majutsushi/tagbar ~/.vim/bundle/tagbar
+
+# Some autocomplete garbage
 git clone https://github.com/vim-scripts/AutoComplPop ~/.vim/bundle/AutoComplPop
-git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
+
+# ctrl-p, which is a file finder. Press ctrl-P and you can search for files
 git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
+
+# Base 16 shells to get the google dark color scheme.
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 cd ~/.vim/bundle
 git clone https://github.com/chriskempson/base16-vim.git
 cd ~
+# For google-dark color scheme for your terminal. If you don't want that, you can get rid of this line.
+base16_google-dark
 
+# Gets you jedi vim, for python autocompletion and more semantic understanding. Could be slow though
+#...for some reason
 mkdir ~/.vim/bundle/jedi-vim
 cd ~/.vim/bundle/jedi-vim
 git submodule update --init
