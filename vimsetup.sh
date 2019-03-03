c568cdc05f5fbc9cfb0e02e90104c16b5ec4131f
Jonathan Xia <jonathanxia@berkeley.edu>
2019-03-03 13:13:00 -0800

    Removed base16 set and fixed jedi vim mkdir
    now uses mkdir -p

--- vimsetup.sh
+++ vimsetup.sh
@@ -2,7 +2,7 @@
 
 # Add pathogen (rtp manager, necessary)
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # Adding Vundle (an rtp manager, also necessary)
 git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
@@ -16,12 +16,10 @@ git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shel
 cd ~/.vim/bundle
 git clone https://github.com/chriskempson/base16-vim.git
 cd ~
-# For google-dark color scheme for your terminal. If you don't want that, you can get rid of this line.
-base16_google-dark
 
 # Gets you jedi vim, for python autocompletion and more semantic understanding. Could be slow though
 #...for some reason
-mkdir ~/.vim/bundle/jedi-vim
+mkdir -p ~/.vim/bundle/jedi-vim
 cd ~/.vim/bundle/jedi-vim
 git submodule update --init
 cd ~
