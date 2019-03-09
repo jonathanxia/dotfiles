3e5406b06e1c4a8f7abee67620eebfec42ed6680
Jonathan Xia <jonathanxia@berkeley.edu>
2019-03-09 13:59:49 -0800

    YCM config looks for correpsonding source file now
    When given a header it will compile the corresponding
    source file to find the implementation of the function.
    
    Could improve somehow. Probably have to look into the linker
    I guess.

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -1,9 +1,30 @@
-def Settings( **kwargs ):
-    if kwargs['filename'].endswith('.cpp'):
-        return {
-            'flags': ['-x', 'c++', '-Wall', '-Wextra', '-Werror']
-        }
-    else:
-        return {
-            'flags': ['-x', 'c', '-Wall', '-Wextra', '-Werror']
-        }
+SOURCE_EXTENSIONS = [ '.cpp', '.cxx', '.cc', '.c', '.m', '.mm' ]
+
+def IsHeaderFile(filename):
+    extension = os.path.splitext(filename)[1]
+    return extension in [ '.h', '.hxx', '.hpp', '.hh' ]
+
+
+def FindCorrespondingSourceFile(filename):
+    if IsHeaderFile(filename):
+        basename = os.path.splitext(filename)[0]
+        for extension in SOURCE_EXTENSIONS:
+            replacement_file = basename + extension
+            if os.path.exists(replacement_file):
+                return replacement_file
+    return filename
+
+
+def Settings(**kwargs):
+    if kwargs['language'] == 'cfamily':
+        filename = FindCorrespondingSourceFile(kwargs['filename'])
+        if kwargs['filename'].endswith('.cpp'):
+            return {
+                    'flags': ['-x', 'c++', '-Wall', '-Wextra', '-Werror'],
+                    'override_filename': filename
+                    }
+        else:
+            return {
+                    'flags': ['-x', 'c', '-Wall', '-Wextra', '-Werror'],
+                    'override_filename': filename
+                    }
