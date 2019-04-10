df3531532497f9b4780d60b0487a47449d280798
Jonathan Xia <jonathanxia@berkeley.edu>
2019-04-10 16:58:27 -0700

    vim destroy script

---
+++ vimdestroy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm -f ~/.vim/autoload/pathogen.vim
+rm -rf ~/.vim/bundle/*
+
+rm -rf ~/.config/base16-shell
+rm -f .vimrc
+rm -f .ycm_extra_conf.py
+rm -f .tmux.conf
+rm -f .gitignore
+rm -rf .cfg
+rm -f vimsetup.sh
+rm -f vimdestroy.sh
+rm -f .vimtags
+rm -f .vimrc_background
+rm -f .viminfo
