bb252560a42a8647cf79c1ca0f32def25d2d9656
jonathanxia <jonathanxia@berkeley.edu>
2019-04-12 04:23:56 +0000

    Fixed vim from source script

--- vim_from_source.sh
+++ vim_from_source.sh
@@ -5,15 +5,18 @@ cd ~
 git clone https://github.com/vim/vim.git
 cd vim
 
-PREFIXNAME=/usr/local    # CHANGE ME!
+PREFIXNAME=$HOME/vim81    # CHANGE ME!
+VIMVERSION=v8.1.0000
+git checkout $VIMVERSION
 # Apparently on Ubuntu you can only use python 2 or python 3. Some garbage like that
+# If you want to use Python 3 you can add --enable-python3interp=yes
+# Also you may need to add this or something --with-python3-config-dir=/usr/lib/python3.5/config
+# Where the config directory is set to something correct
 ./configure --with-features=huge \
             --enable-multibyte \
             --enable-rubyinterp=yes \
             --enable-pythoninterp=yes \
-            --with-python-config-dir=/usr/lib/python2.7/config \ # CHANGE TO THE RIGHT THING!
-            --enable-python3interp=yes \
-            --with-python3-config-dir=/usr/lib/python3.5/config \ # CHANGE TO THE RIGHT THING!
+            --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
             --enable-perlinterp=yes \
             --enable-luainterp=yes \
             --enable-gui=gtk2 \
@@ -34,7 +37,10 @@ tar xvzf $CMAKENAME.tar.gz
 # clean it up
 rm -f $CMAKENAME.tar.gz
 
-#-----------------Reinstall YCM-------------
-export PATH="$HOME/$CMAKENAME/bin"
+#-----------------Reinstall YCM-------------------
+export PATH="$HOME/$CMAKENAME/bin:$PATH"
 cd ~/.vim/bundle/YouCompleteMe
 ./install.py --clang-completer --java-completer
+
+#-----------------Remove CMake------------------
+rm -rf $CMAKENAME
