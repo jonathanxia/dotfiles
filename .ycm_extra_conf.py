214243d1be855946e09628e9060a9d07244c4dd6
Jonathan Xia <jonathanxia@berkeley.edu>
2019-11-10 01:45:36 -0500

    remove hardcoded paths

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -29,12 +29,4 @@ def Settings(**kwargs):
                     'flags': ['-x', 'c', '-Wall', '-Wextra', '-Werror'],
                     'override_filename': filename
                     }
-    else:
-        return {
-                'interpreter_path': '/usr/local/bin/python',
-                'sys_path': [
-                    '/prod/tools/base/lib/python/',
-                    '/prod/tools/eq/src/lib/python/',
-                    '/codemill/xiaj/algor/exploded_algor/'
-                    ]
-                }
+
