f4dcae13f0775fedb24b06c7b64fac83356bfc40
Jonathan Xia <xiaj@deshaw.com>
2019-11-13 17:42:14 -0500

    [ALL] Remove cruft old code

--- .promptline.sh
+++ .promptline.sh
@@ -78,24 +78,6 @@ function __promptline_cwd {
   local cwd="${PWD/#$HOME/$tilde}"
   local oldcwd=$cwd
 
-  # get first char of the path, i.e. tilde or slash
-  [[ -n ${ZSH_VERSION-} ]] && first_char=$cwd[1,1] || first_char=${cwd::1}
-
-  # remove leading tilde
-  cwd="${cwd#\~}"
-
-  while [[ "$cwd" == */* && "$cwd" != "/" ]]; do
-    # pop off last part of cwd
-    local part="${cwd##*/}"
-    cwd="${cwd%/*}"
-
-    formatted_cwd="$dir_sep$part$formatted_cwd"
-    part_count=$((part_count+1))
-
-    [[ $part_count -eq $dir_limit ]] && first_char="$truncation" && break
-  done
-
-  #printf "%s" "$first_char$formatted_cwd"
   printf "%s" "$oldcwd"
 }
 
