28052e4487af280f63f3df63a478d028c0f912a6
Jonathan Xia <xiaj@deshaw.com>
2019-12-24 23:57:16 -0500

    Cleaned up promptline.sh

--- .promptline.sh
+++ .promptline.sh
@@ -16,42 +16,68 @@ function __promptline_last_exit_code {
 
   printf "%s" "$last_exit_code"
 }
+
+function __promptline_default_wrapper {
+    # A function that calls the wrapper but has default arguments
+    # This takes advantage of the dynamically scoped nature of bash. It
+    # assumes parameters 2 and 3 are $slice_prefix and $slice_suffix
+    __promptline_wrapper "$1" "$slice_prefix" "$slice_suffix" && \
+        { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
+}
+
+
 function __promptline_ps1 {
   local slice_prefix slice_empty_prefix slice_joiner slice_suffix is_prompt_empty=1
 
   # section "a" header
-  slice_prefix="${a_bg}${sep}${a_fg}${a_bg}${space}" slice_suffix="$space${a_sep_fg}" slice_joiner="${a_fg}${a_bg}${alt_sep}${space}" slice_empty_prefix="${a_fg}${a_bg}${space}"
+  slice_prefix="${a_bg}${sep}${a_fg}${a_bg}${space}"
+  slice_suffix="$space${a_sep_fg}"
+  slice_joiner="${a_fg}${a_bg}${alt_sep}${space}"
+  slice_empty_prefix="${a_fg}${a_bg}${space}"
   [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
   # section "a" slices
-  __promptline_wrapper "\t" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
+  __promptline_default_wrapper "\t"
 
   # section "b" header
-  slice_prefix="${b_bg}${sep}${b_fg}${b_bg}${space}" slice_suffix="$space${b_sep_fg}" slice_joiner="${b_fg}${b_bg}${alt_sep}${space}" slice_empty_prefix="${b_fg}${b_bg}${space}"
+  slice_prefix="${b_bg}${sep}${b_fg}${b_bg}${space}"
+  slice_suffix="$space${b_sep_fg}"
+  slice_joiner="${b_fg}${b_bg}${alt_sep}${space}"
+  slice_empty_prefix="${b_fg}${b_bg}${space}"
   [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
   # section "b" slices
-  __promptline_wrapper "$USER@$(__promptline_host)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
+  __promptline_default_wrapper "$USER@$(__promptline_host)"
 
   # section "c" header
-  slice_prefix="${c_bg}${sep}${c_fg}${c_bg}${space}" slice_suffix="$space${c_sep_fg}" slice_joiner="${c_fg}${c_bg}${alt_sep}${space}" slice_empty_prefix="${c_fg}${c_bg}${space}"
+  slice_prefix="${c_bg}${sep}${c_fg}${c_bg}${space}"
+  slice_suffix="$space${c_sep_fg}"
+  slice_joiner="${c_fg}${c_bg}${alt_sep}${space}"
+  slice_empty_prefix="${c_fg}${c_bg}${space}"
   [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
   # section "c" slices
-  __promptline_wrapper "$(__promptline_cwd)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
+  __promptline_default_wrapper "$(__promptline_cwd)"
 
   # section "y" header
-  slice_prefix="${y_bg}${sep}${y_fg}${y_bg}${space}" slice_suffix="$space${y_sep_fg}" slice_joiner="${y_fg}${y_bg}${alt_sep}${space}" slice_empty_prefix="${y_fg}${y_bg}${space}"
+  slice_prefix="${y_bg}${sep}${y_fg}${y_bg}${space}"
+  slice_suffix="$space${y_sep_fg}"
+  slice_joiner="${y_fg}${y_bg}${alt_sep}${space}"
+  slice_empty_prefix="${y_fg}${y_bg}${space}"
   [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
   # section "y" slices
-  __promptline_wrapper "$(__promptline_vcs_branch)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
+  __promptline_default_wrapper "$(__promptline_vcs_branch)"
 
   # section "warn" header
-  slice_prefix="${warn_bg}${sep}${warn_fg}${warn_bg}${space}" slice_suffix="$space${warn_sep_fg}" slice_joiner="${warn_fg}${warn_bg}${alt_sep}${space}" slice_empty_prefix="${warn_fg}${warn_bg}${space}"
+  slice_prefix="${warn_bg}${sep}${warn_fg}${warn_bg}${space}"
+  slice_suffix="$space${warn_sep_fg}"
+  slice_joiner="${warn_fg}${warn_bg}${alt_sep}${space}"
+  slice_empty_prefix="${warn_fg}${warn_bg}${space}"
   [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
   # section "warn" slices
-  __promptline_wrapper "$(__promptline_last_exit_code)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
+  __promptline_default_wrapper "$(__promptline_last_exit_code)"
 
   # close sections
   printf "%s" "${reset_bg}${sep}$reset$space"
 }
+
 function __promptline_vcs_branch {
   local branch
   local branch_symbol=" "
